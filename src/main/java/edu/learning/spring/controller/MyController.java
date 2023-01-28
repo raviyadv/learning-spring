fa64e26794540d4e3f3425924fe89528226b4ea7
Ravi Yadav <tech.raviyadav@gmail.com>
2023-01-28 22:02:34 +0530

    added

---
+++ src/main/java/edu/learning/spring/controller/MyController.java
@@ -0,0 +1,14 @@
+package edu.learning.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class MyController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
